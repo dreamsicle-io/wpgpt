1b57e3f490f54833a91eafec5f7048ec6d535991
Noah Mason <noahmason23@gmail.com>
2023-04-01 19:43:16 -0400

    Adds get_current_url util and refactors get_thickbox_url util to account for '?" in URL.

--- includes/class-wpgpt-utils.php
+++ includes/class-wpgpt-utils.php
@@ -18,6 +18,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WPGPT_Utils {
 
 	/**
+	 * Get Current URL
+	 *
+	 * @since 0.3.0
+	 * @return string The current URL.
+	 */
+	public static function get_current_url(): string {
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$params = ! empty( $_GET ) ? wp_unslash( $_GET ) : array();
+		$server = ! empty( $_SERVER ) ? wp_unslash( $_SERVER ) : array();
+		$base   = ! empty( $server['REQUEST_URI'] ) ? $server['REQUEST_URI'] : '';
+		return add_query_arg( $params, home_url( $base ) );
+	}
+
+	/**
 	 * Get Thickbox URL
 	 *
 	 * @since 0.1.0
@@ -25,13 +39,23 @@ class WPGPT_Utils {
 	 * @return string The thickbox URL.
 	 */
 	public static function get_thickbox_url( array $params ): string {
-		$params = wp_parse_args(
+		$current_url  = self::get_current_url();
+		$params       = wp_parse_args(
 			$params,
 			array(
 				'inlineId' => 'wpgpt-thickbox',
 				'width'    => 753,
 			)
 		);
-		return '#TB_inline&' . build_query( $params );
+		$param_string = build_query( $params );
+
+		// Inline thickbox links break when a `?` sep is aready in the URL,
+		// so use `&` as the sep instead if `?` is found in the URL.
+		$sep = '?';
+		if ( str_contains( $current_url, '?' ) ) {
+			$sep = '&';
+		}
+
+		return "#TB_inline{$sep}{$param_string}";
 	}
 }
