6a0245c8db2032662b907170520cca9d25843c63
Noah Mason <noahmason23@gmail.com>
2023-04-01 11:55:27 -0400

    Updates requires and initializers.

--- wpgpt.php
+++ wpgpt.php
@@ -18,13 +18,15 @@
 define( 'WPGPT_PATH', plugin_dir_path( __FILE__ ) );
 
 require WPGPT_PATH . '/vendor/autoload.php';
-require WPGPT_PATH . '/includes/class-wpgpt.php';
+require WPGPT_PATH . '/includes/class-wpgpt-api.php';
 require WPGPT_PATH . '/includes/class-wpgpt-settings.php';
 require WPGPT_PATH . '/includes/class-wpgpt-post-generator.php';
+require WPGPT_PATH . '/includes/class-wpgpt-elaborator.php';
 require WPGPT_PATH . '/includes/class-wpgpt-caption-generator.php';
 require WPGPT_PATH . '/includes/class-wpgpt-utils.php';
 
-add_action( 'plugins_loaded', array( new WPGPT(), 'init' ), 0 );
+add_action( 'plugins_loaded', array( new WPGPT_API(), 'init' ), 0 );
 add_action( 'plugins_loaded', array( new WPGPT_Settings(), 'init' ), 0 );
 add_action( 'plugins_loaded', array( new WPGPT_Post_Generator(), 'init' ), 0 );
+add_action( 'plugins_loaded', array( new WPGPT_Elaborator(), 'init' ), 0 );
 add_action( 'plugins_loaded', array( new WPGPT_Caption_Generator(), 'init' ), 0 );
