a567a4345d6b30d002f8e4421c0db08a19192acc
Noah Mason <noahmason23@gmail.com>
2023-04-01 16:13:47 -0400

    Comments class-wpgpt-api.php file.

--- includes/class-wpgpt-api.php
+++ includes/class-wpgpt-api.php
@@ -1,18 +1,89 @@
 <?php
+/**
+ * WPGPT API
+ *
+ * @package wpgpt
+ * @since 0.1.0
+ */
 
 use OpenAI\Client;
 
+/**
+ * WPGPT API
+ *
+ * @since 0.1.0
+ */
 class WPGPT_API {
 
+	/**
+	 * API Key
+	 *
+	 * @var string $api_key The OpenAI API key.
+	 * @since 0.1.0
+	 */
 	private string $api_key;
+
+	/**
+	 * Default Max Tokens
+	 *
+	 * @var int $default_max_tokens The default max tokens for the OpenAI API request.
+	 * @since 0.1.0
+	 */
 	private int $default_max_tokens;
+
+	/**
+	 * Default Temperature
+	 *
+	 * @var float $default_temperature The default temperature for the OpenAI API request.
+	 * @since 0.1.0
+	 */
 	private float $default_temperature;
+
+	/**
+	 * Default Presence Penalty
+	 *
+	 * @var float $default_presence_penalty The default presence penalty for the OpenAI API request.
+	 * @since 0.1.0
+	 */
 	private float $default_presence_penalty;
+
+	/**
+	 * Default Frequency Penalty
+	 *
+	 * @var float $default_frequency_penalty The default frequency penalty for the OpenAI API request.
+	 * @since 0.1.0
+	 */
 	private float $default_frequency_penalty;
+
+	/**
+	 * Default model
+	 *
+	 * @var string $default_model The default model for the OpenAI API request.
+	 * @since 0.1.0
+	 */
 	private string $default_model;
+
+	/**
+	 * Default choices
+	 *
+	 * @var int $default_choices The default number of choices for the OpenAI API response.
+	 * @since 0.1.0
+	 */
 	private int $default_choices;
+
+	/**
+	 * Client
+	 *
+	 * @var Client $client The OpenAI/Client instance.
+	 * @since 0.1.0
+	 */
 	private Client $client;
 
+	/**
+	 * Construct
+	 *
+	 * @since 0.1.0
+	 */
 	public function __construct() {
 		$this->api_key                   = get_option( 'wpgpt_openai_api_key', '' );
 		$this->default_max_tokens        = get_option( 'wpgpt_default_max_tokens', 3000 );
@@ -24,10 +95,22 @@ class WPGPT_API {
 		$this->client                    = OpenAI::client( $this->api_key );
 	}
 
+	/**
+	 * Init
+	 *
+	 * @since 0.1.0
+	 * @return void
+	 */
 	public function init() {
 		add_action( 'rest_api_init', array( $this, 'register_rest_routes' ), 10 );
 	}
 
+	/**
+	 * Register REST Routes
+	 *
+	 * @since 0.1.0
+	 * @return void
+	 */
 	public function register_rest_routes() {
 		register_rest_route(
 			'wpgpt/v1',
@@ -70,7 +153,14 @@ class WPGPT_API {
 		);
 	}
 
-	public function generate( WP_REST_Request $request ) {
+	/**
+	 * Generate
+	 *
+	 * @since 0.1.0
+	 * @param WP_REST_Request $request An incoming API request instance.
+	 * @return WP_REST_Response An outgoing API response instance.
+	 */
+	public function generate( WP_REST_Request $request ): WP_REST_Response {
 
 		$model             = $request->get_param( 'model' );
 		$choices           = $request->get_param( 'choices' );
