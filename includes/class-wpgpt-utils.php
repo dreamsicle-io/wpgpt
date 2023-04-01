b5151727c31f066c2950801d1534fa5d709e7e56
Noah Mason <noahmason23@gmail.com>
2023-04-01 16:44:11 -0400

    Comments the class-wpgpt-utils.php file.

--- includes/class-wpgpt-utils.php
+++ includes/class-wpgpt-utils.php
@@ -1,19 +1,37 @@
 <?php
+/**
+ * WPGPT Utils
+ *
+ * @package wpgpt
+ * @since 0.1.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * WPGPT Utils
+ *
+ * @since 0.1.0
+ */
 class WPGPT_Utils {
 
-	public static function get_thickbox_url( array $args ) {
-		$args = wp_parse_args(
-			$args,
+	/**
+	 * Get Thickbox URL
+	 *
+	 * @since 0.1.0
+	 * @param array $params An array of params to be used in the thickbox URL.
+	 * @return string The thickbox URL.
+	 */
+	public static function get_thickbox_url( array $params ): string {
+		$params = wp_parse_args(
+			$params,
 			array(
 				'inlineId' => 'wpgpt-thickbox',
 				'width'    => 753,
 			)
 		);
-		return '#TB_inline&' . build_query( $args );
+		return '#TB_inline&' . build_query( $params );
 	}
 }
